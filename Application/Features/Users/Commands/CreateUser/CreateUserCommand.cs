811a442a5009568e35ffe5e5fa93f25c180b6b5e
Talha Toğuşlu <talha.toguslu@outlook.com>
2022-09-22 12:29:12 +0300

    Add: user register command

---
+++ Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.ProgramingLanguages.Rules;
+using Application.Features.Users.Rules;
+using Application.Services;
+using AutoMapper;
+using Core.Security.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserCommand: IRequest<UserForRegisterDto>
+    {
+        public UserForRegisterDto User { get; set; }
+
+        public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserForRegisterDto>
+        {
+            private readonly IUserService _userRepository;
+            private readonly IMapper _mapper;
+            private readonly UserBusinessRules _userBusinessRules;
+
+            public CreateUserCommandHandler(IUserService userRepository,
+                IMapper mapper,
+                UserBusinessRules userBusinessRules)
+            {
+                _userRepository = userRepository;
+                _mapper = mapper;
+                _userBusinessRules = userBusinessRules;
+            }
+
+            public Task<UserForRegisterDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Services;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UserRepository : EfRepositoryBase<User, BaseDBContext>, IUserService
+    {
+        public UserRepository(BaseDBContext context) : base(context)
+        {
+        }
+    }
+}
